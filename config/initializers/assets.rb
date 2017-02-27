edf7fd925dfe43aa02ab3bf534e3c24161c6e698
Vlad Dzinko <dzinko23@gmail.com>
2017-02-27 23:38:25 +0200

    Try fix font bug#3, back to old version

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,7 +12,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( bootstrap.min.css )
 Rails.application.config.assets.precompile += %w( customize.css )
 Rails.application.config.assets.precompile += %w( style.css )
-Rails.application.config.assets.precompile += %w( fonts.css )
+
 
 Rails.application.config.assets.precompile += %w( alert.js )
 Rails.application.config.assets.precompile += %w( jquery.sequence-min.js )
